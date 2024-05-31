09f22d2b144d56db2ef20e09b96bc1e0e99eb080
minjeongss <dotoriido@gmail.com>
2024-06-01 01:22:34 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,36 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>개발자, 김민정입니다. 🐢</h1>
+        <div>기록을 중요하게 생각합니다.</div>
+        <div>다양한 분야를 모험하는 것을 즐깁니다.</div>
+      <hr />
+
+      <h2>About ME</h2>
+      <h3>Contact</h3>
+        <div><a href="https://github.com/minjeongss">Github</a></div>
+        <div><a href="https://jeonge.tistory.com/">Tech Blog</a></div>
+        <div><a href=""></a></div>
+
+      <h3>Education</h3>
+        <div>Inha University, 컴퓨터공학 학사 (2020.03 ~ )</div>
+      <hr />
+
+      <h2>About Tech Stack</h2>
+      <h3>Language Skill</h3>
+        <div>JavaScript(ES6+), TypeScript, React.js, HTML, CSS</div>
+        <div>Node.js, Python, ROS, C/C++, Java</div>
+      <h3>Tool Skill</h3>  
+        <div>Linux, Tableau, Unity</div>
+      <hr />
+      
+      <h2>About Project</h2>
+      <div>투비컨티뉴✈️</div>
+      
+    </>
+  )
+}
+
+export default App
