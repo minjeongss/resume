13aafe196b6fc00a5c77dc66f07f6142a39f8299
minjeongss <dotoriido@gmail.com>
2024-06-06 04:48:09 +0900

    feat: styled-component로 font 적용 분리

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {createGlobalStyle} from 'styled-components';
+import './font.css'
 import './index.css'
 
+const GlobalFont=createGlobalStyle`
+  #root{
+    font-family:"NanumSquare";
+  }
+`;
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <GlobalFont />
     <App />
   </React.StrictMode>,
 )
