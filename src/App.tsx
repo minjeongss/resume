d4e805f4f411e2ecafcde79a52d9fe6fb2fb022f
minjeongss <dotoriido@gmail.com>
2024-06-06 05:07:41 +0900

    feat: 전역적 사용되는 버튼 CSS 초기 설정
    - focus시 outline 없도록 설정
    - hover시 글자만 색상 변경되도록 설정
    - 버튼 공간 존재하도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -47,6 +47,10 @@ function App() {
           <span>웹 제작 프로젝트</span>
           <button>더보기</button>
         </div>
+        <div>
+          <span>웹 제작 프로젝트</span>
+          <button>더보기</button>
+        </div>
       </div>
       
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -34,19 +34,22 @@ const GlobalFont=createGlobalStyle`
     border-radius: 8px;
     border: 1px solid transparent;
     padding: 0.6em 1.2em;
+    margin: 0.5em 1em;
     font-size: 1em;
-    font-weight: 500;
     font-family: inherit;
   
     cursor: pointer;
     transition: border-color 0.25s;
   }
   button:hover {
-    border-color: #646cff;
+    color: #646cff;
   }
   button:focus,
   button:focus-visible {
     outline: 4px auto -webkit-focus-ring-color;
+  }
+  button:focus{
+    outline:none;
   }
 `;
 ReactDOM.createRoot(document.getElementById('root')!).render(
