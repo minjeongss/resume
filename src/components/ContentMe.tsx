3486e12d0d4ae0ea2747f61e46486bb45c543e8a
minjeongss <dotoriido@gmail.com>
2024-06-08 16:54:40 +0900

    feat: Contact 버튼 글자 넘치지 않도록 설정

--- src/components/ContentMe.tsx
+++ src/components/ContentMe.tsx
@@ -1,16 +1,34 @@
 import styled from "styled-components";
-const Button=styled.button`
-    width:30%;
 
+const ContainerButton=styled.div`
+    display:flex;
+    flex-wrap:wrap;
+    justify-content:space-between;
+    width:100%;
+`;
+const Button=styled.button`
+flex: 1 1 calc(33.33% - 10px);
+margin: 5px;
+padding: 10px;
+text-align: center;
+white-space: nowrap; // 텍스트 줄바꿈 방지
+text-overflow: ellipsis; // 텍스트가 넘치면 생략 부호 표시
+
+a {
+    color: inherit;
+    text-decoration: none;
+}
 `;
 
 const ContentMe = () => {
     return (
         <>
             <h3>Contact</h3>
-                <Button><a href="https://github.com/minjeongss" target='_blank'>💻️ Github</a></Button>
-                <Button><a href="https://jeonge.tistory.com/" target='_blank'>📚️ Tech Blog</a></Button>
-                <Button><a href="mailto:dotoriido@gmail.com" target='_blank'>📨 Email</a></Button>
+                <ContainerButton>
+                    <Button><a href="https://github.com/minjeongss" target='_blank'>💻️ Github</a></Button>
+                    <Button><a href="https://jeonge.tistory.com/" target='_blank'>📚️ Tech Blog</a></Button>
+                    <Button><a href="mailto:dotoriido@gmail.com" target='_blank'>📨 Email</a></Button>
+                </ContainerButton>
             <h3>Education</h3>
         <div>Inha University, 컴퓨터공학 학사 (2020.03 ~ )</div>
         </>
