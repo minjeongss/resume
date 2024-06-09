18a8eecd49ecd03f4aadce9a5db4f0acd1b32012
minjeongss <dotoriido@gmail.com>
2024-06-09 16:45:46 +0900

    feat: Project부분 자세한 내용 기술

--- src/components/ContentProject.tsx
+++ src/components/ContentProject.tsx
@@ -56,24 +56,24 @@ interface Project{
 
 const ContentProject = () => {
     const project=[
-        {type:"project", title:"[교내 동아리 IGRUS] 팀 Watfle",detail:"영화 리뷰 웹페이지",position:"개발(프론트엔드)",duration:"2020-2021",tech:"JS, CSS, HTML",link:"https://github.com/minjeongss/web-Watfle",
+        {type:"project", title:"[교내 동아리 IGRUS] 팀 Watfle",detail:"영화 리뷰 웹페이지 제작",position:"개발(프론트엔드)",duration:"2020-2021",tech:"JS, CSS, HTML",link:"https://github.com/minjeongss/web-Watfle",
             content:"",takeaway:""},
         {type:"study", title:"[교내 빅데이터 학회 INBIG] 소모임 데이터 시각화",detail:"데이터 분석",position:"개발",duration:"2021",tech:"Python, Tableau",link:"https://github.com/minjeongss/Data-Visualization",
             content:"",takeaway:""},
         {type:"company", title:"[환경 교육 기업 에코드인] 컨텐츠 개발 팀원",detail:"컨텐츠 시나리오 제작",position:"PD",duration:"2021-2022",tech:"Unity",link:"https://ecode-in.com/",
             content:"",takeaway:""},
-        {type:"project", title:"[개인 팀] SosoVillage",detail:"2D 인디게임",position:"개발,디자인",duration:"2022",tech:"C#, Unity",link:"https://github.com/JeongHyunJi/SosoVillage",
+        {type:"project", title:"[개인 팀] SosoVillage",detail:"2D 인디게임 제작",position:"개발,디자인",duration:"2022",tech:"C#, Unity",link:"https://github.com/JeongHyunJi/SosoVillage",
             content:"",takeaway:""},
-        {type:"project", title:"[연합 동아리 UMC] 팀 오늘뭐하니",detail:"놀거리 플랫폼 서비스",position:"개발(백엔드)",duration:"2023",tech:"Node.js(ES6)",link:"https://github.com/UMC-LetsDo/what-you-do-today-back",
+        {type:"project", title:"[연합 동아리 UMC] 팀 오늘뭐하니",detail:"놀거리 플랫폼 서비스 제작",position:"개발(백엔드)",duration:"2023",tech:"Node.js(ES6)",link:"https://github.com/UMC-LetsDo/what-you-do-today-back",
             content:"",takeaway:""},
-        {type:"project", title:"[해커톤 준비] 팀 귀농귀농",detail:"귀농 플랫폼 서비스",position:"개발(프론트엔드)",duration:"2023",tech:"React.js(ES6)",link:"https://github.com/GwinongGwinong/frontend",
+        {type:"project", title:"[해커톤 준비] 팀 귀농귀농",detail:"귀농 플랫폼 서비스 제작",position:"개발(프론트엔드)",duration:"2023",tech:"React.js(ES6)",link:"https://github.com/GwinongGwinong/frontend",
             content:"",takeaway:""},
-        {type:"study", title:"[교내 동아리 IGRUS] 소모임 Connect",detail:"리액트 강의",position:"강의 보조 및 피드백",duration:"2023",tech:"React.js(ES6)",link:"https://github.com/Connect-Organization/2023-ReactStudy-Jeong",
+        {type:"study", title:"[교내 동아리 IGRUS] 소모임 Connect",detail:"리액트 강의 진행",position:"강의 보조 및 피드백",duration:"2023",tech:"React.js(ES6)",link:"https://github.com/Connect-Organization/2023-ReactStudy-Jeong",
             content:"",takeaway:""},
-        {type:"project", title:"[연합 동아리 GDSC] 팀 나날이",detail:"날씨별 옷 추천 플랫폼 서비스",position:"개발(프론트엔드)",duration:"2023",tech:"React.js(ES6)",link:"https://github.com/Nanali-cody/frontend",
+        {type:"project", title:"[연합 동아리 GDSC] 팀 나날이",detail:"날씨별 옷 추천 플랫폼 서비스 제작",position:"개발(프론트엔드)",duration:"2023",tech:"React.js(ES6)",link:"https://github.com/Nanali-cody/frontend",
             content:"",takeaway:""},
-        {type:"company", title:"[공간지능 및 로보틱스 연구실] 학부연구생",detail:"다층 환경 주행을 위한 모바일 매니퓰레이터 및 내비게이션 기술",position:"개발(제어)",duration:"2023-2024",tech:"ROS1, Python",link:"https://sites.google.com/view/sparo",
-            content:"2023 동계 학부 연구생 인턴십 프로그램, 2024 1학기 학부 연구생 인턴십 프로그램에 참여했습니다. ",takeaway:"모바일 로봇 제어"},
+        {type:"company", title:"[공간지능 및 로보틱스 연구실] 학부연구생",detail:"다층 환경 주행을 위한 모바일 매니퓰레이터 및 내비게이션 기술 구현",position:"개발(제어)",duration:"2023-2024",tech:"ROS1, Python",link:"https://sites.google.com/view/sparo",
+            content:"2023 동계 학부 연구생 인턴십 프로그램, 2024 1학기 학부 연구생 인턴십 프로그램에 참여했습니다. 구현한 기술을 바탕으로 2024 제39회 제어로봇시스템학회 학술대회에 논문을 투고하는 쾌거를 거두었습니다.",takeaway:"모바일 로봇 제어"},
         {type:"company", title:"[로봇 제작 기업 유진로봇] 인턴사원",detail:"청소로봇 개발 테스트 및 UI 시나리오 검증",position:"개발(테스트)",duration:"2024",tech:"Linux",link:"https://yujinrobot.com/",
             content:"청소로봇 연구실 소프트웨어 부서에서 현장실습을 진행했습니다. ",takeaway:"테스트 역량, 리눅스 사용 능력 향상, 시니어 엔지니어와의 소통"},
     ];
@@ -120,7 +120,7 @@ const ContentProject = () => {
                     })
                 }
                 </ContainerWrapper>
-                <H3>| 프로젝트 연계</H3>
+                <H3>| 프로젝트 연계: 웹앱, 게임 제작</H3>
                 <ContainerWrapper>
                 {
                     project.map((elem)=>{
@@ -141,7 +141,7 @@ const ContentProject = () => {
                     })
                 }
                 </ContainerWrapper>
-                <H3>| 스터디 연계</H3>
+                <H3>| 스터디 연계: 리액트 강의 진행, 데이터 분석</H3>
                 <ContainerWrapper>
                 {
                     project.map((elem)=>{
